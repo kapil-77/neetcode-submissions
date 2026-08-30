1fc006fc2fc94dc0c023be4747c767f59ca68024
Kapil Bhatt <51691532+kapil-77@users.noreply.github.com>
2026-08-30 23:57:19 +0530

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.js
@@ -0,0 +1,10 @@
+class Solution {
+    getConcatenation(nums) {
+        const n = nums.length
+        for (let i = 0; i < n; i++) {
+            nums.push(nums[i])
+        }
+        return nums
+
+    }
+}
