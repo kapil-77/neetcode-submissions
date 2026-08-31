963f250f8c0e98ea25114cffce9bef07589bb8a9
Kapil Bhatt <51691532+kapil-77@users.noreply.github.com>
2026-08-31 13:17:15 +0530

    Add: concatenation-of-array - submission-2

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-2.js
@@ -0,0 +1,11 @@
+class Solution {
+    getConcatenation(nums) {
+        let n = nums.length;
+        let ans = new Array(2*n);
+
+        for(let i = 0; i < n; i++) {
+            ans[i] = ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+}
