53b6c85e3543f652a73984865667a4f90d36483f
Kapil Bhatt <51691532+kapil-77@users.noreply.github.com>
2026-09-01 21:37:46 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.js
@@ -0,0 +1,10 @@
+class Solution {
+    isValid(s) {
+        while (s.includes('()') || s.includes('{}') || s.includes('[]')) {
+            s = s.replace('()', '');
+            s = s.replace('{}', '');
+            s = s.replace('[]', '');
+        }
+        return s === '';
+    }
+}
