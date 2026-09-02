0b0ef071a91ff19e32a7343b645650c39edaa7e6
Kapil Bhatt <51691532+kapil-77@users.noreply.github.com>
2026-09-02 22:42:07 +0530

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.js
@@ -0,0 +1,12 @@
+class Solution {
+    longestCommonPrefix(strs) {
+        for (let i = 0; i < strs[0].length; i++) {
+            for (let s of strs) {
+                if (i === s.length || s[i] !== strs[0][i]) {
+                    return s.slice(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
